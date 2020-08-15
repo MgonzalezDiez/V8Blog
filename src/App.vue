be7110217e2913e768d00ca95df18d76016ea83b
Marco Antonio González <mgonzalezdiez@gmail.com>
2020-08-15 11:53:47 -0400

    Fin proyecto V6Blog

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,28 @@
 <template>
-  <div>
-    <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand">V8</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
-            <div
-                class="collapse navbar-collapse" id="navbarResponsive">
-                <!-- MENU DE NAVEGACION -->
-                <ul class="nav navbar-nav ml-auto" id="menu-v8">
-                    <li class="nav-item" role="presentation">
-                      <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A CON RUTA NOMBRADA -->
-                      <a href="/">portada</a> 
-                    </li>
-                    <li class="nav-item" role="presentation">
-                      <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A CON RUTA NOMBRADA -->
-                      <a href="/sobremi">sobre mí</a> 
-                    </li>
-                    <li class="nav-item" role="presentation">
-                      <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A CON RUTA ESTÁTICA -->
-                      <a href="/">contacto</a> 
-                    </li>
-                    <li class="nav-item" role="presentation">
-                      <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A RUTA ESTÁTICA -->
-                      <a href="/post/1">último post</a> 
-                    </li>
-                </ul>
+  <div id="app">
+    <nav class="navbar navbar-bg-dark navbar-expand-lg  fixed-top" id="mainNav">
+      <div class="container"><a class="navbar-brand">V8</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item" role="presentation">
+              <router-link to="/">Home</router-link>
+            </li>
+            <li class="nav-item" role="presentation">
+              <router-link to="/about">Sobre Mí</router-link>
+            </li>
+            <li class="nav-item" role="presentation">
+              <router-link to="/contact">Contacto</router-link>
+            </li>
+            <li class="nav-item" role="presentation">
+              <router-link to="/post/1">Último Post</router-link>
+            </li>
+          </ul>
         </div>
-        </div>
+      </div>
     </nav>
-    <!-- ROUTER VIEW QUE CARGARÁ LAS VISTAS (INICIO, SOBRE MÍ, CONTACTO, ETC.) -->
-    <!-- <router-view></router-view> -->
+    <router-view></router-view>
   </div>
-  
 </template>
-
-<script>
-
-export default {
-
-}
-</script>
 
 <style scoped>
 #app {
@@ -46,7 +31,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;  
+
 }
 #menu-v8 a{
   text-shadow: 1px 1px #000;
